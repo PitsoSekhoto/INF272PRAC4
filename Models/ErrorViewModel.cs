ca64fd246df45549a32acf24bf127f5c70dcab13
pitsosekhoto <pitsosekhoto@PITSOSEKHOT69AF>
2024-04-21 22:42:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INF272PRAC4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
